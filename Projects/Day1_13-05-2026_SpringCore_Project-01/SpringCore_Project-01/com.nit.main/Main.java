9f501a5800191112251ccb2a6fed1ca98f4a665e
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-05-26 23:10:31 +0530

    Add Main class to initialize Spring application
    This file contains the main class for the Spring Core project, which initializes the application context and retrieves an Employee bean to display its details.

---
+++ Projects/Day1_13-05-2026_SpringCore_Project-01/SpringCore_Project-01/com.nit.main/Main.java
@@ -0,0 +1,29 @@
+package com.nit.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.nit.beans.Employee;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String config_file="/com/nit/resources/applicationContext.xml";
+		
+		ApplicationContext context=new ClassPathXmlApplicationContext(config_file);
+		Employee emp=(Employee) context.getBean("empId");
+		
+		emp.display();
+		
+		
+         
+	}
+
+}
+
+OUTPUT
+------
+Emp ID is: 101
+Emp NAME is: Subham
+Emp SALARY is: 50000.0
+Emp DEPARTMENT is: Devloper
