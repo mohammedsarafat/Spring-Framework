c12ba5ebc8fc01bf6c68a572f549f726c0a7f681
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-05-27 00:05:23 +0530

    Add Customer class with properties and methods

---
+++ Projects/Day02_14-05-2026_SpringCore_Project-02/SpringCore_Project-02/com.nit.beans/Customer.java
@@ -0,0 +1,37 @@
+com.nit.beanspackage com.nit.beans;
+
+public class Customer {
+	private int id;
+	private String name;
+	private String addr;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + ", addr=" + addr + "]";
+	}
+	public void display() {
+		IO.println("Customer Id is: "+id);
+		IO.println("Customer Name is: "+name);
+		IO.println("Customer Dept is: "+addr);
+	}
+}
