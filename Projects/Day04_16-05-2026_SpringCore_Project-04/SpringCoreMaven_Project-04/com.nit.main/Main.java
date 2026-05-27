51f9de920184a4291e581ce5c2ec552e511326d3
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-05-28 00:02:46 +0530

    Create Main class to run Spring application
    Initialize Main class for Spring application and load context.

---
+++ Projects/Day04_16-05-2026_SpringCore_Project-04/SpringCoreMaven_Project-04/com.nit.main/Main.java
@@ -0,0 +1,16 @@
+package com.nit.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.nit.beans.Employee;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Employee emp = (Employee) context.getBean("empId");
+		emp.display();
+	}
+
+}
