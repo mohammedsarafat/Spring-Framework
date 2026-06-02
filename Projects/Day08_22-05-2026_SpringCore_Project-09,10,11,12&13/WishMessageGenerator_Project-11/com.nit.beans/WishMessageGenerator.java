74d8b309f1581606445e62614566411d66f7233a
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-06-03 00:00:35 +0530

    Add WishMessageGenerator class for greeting messages

---
+++ Projects/Day08_22-05-2026_SpringCore_Project-09,10,11,12&13/WishMessageGenerator_Project-11/com.nit.beans/WishMessageGenerator.java
@@ -0,0 +1,25 @@
+package com.nit.beans;
+
+import java.time.LocalDateTime;
+
+public class WishMessageGenerator {
+
+    private LocalDateTime currentDateTime;
+
+    public void setCurrentDateTime(LocalDateTime currentDateTime) {
+        this.currentDateTime = currentDateTime;
+    }
+
+    public String generateWishMessage() {
+
+        int hour = currentDateTime.getHour();
+
+        if (hour < 12) {
+            return "Good Morning";
+        } else if (hour < 18) {
+            return "Good Afternoon";
+        } else {
+            return "Good Evening";
+        }
+    }
+}
