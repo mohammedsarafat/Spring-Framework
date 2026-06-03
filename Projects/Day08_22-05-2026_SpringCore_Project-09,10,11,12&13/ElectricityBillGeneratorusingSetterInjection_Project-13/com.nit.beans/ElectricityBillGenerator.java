e772de92b923bdc8dd2cb260434a0f47decffd4a
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-06-03 23:41:52 +0530

    Add ElectricityBillGenerator class for bill calculation

---
+++ Projects/Day08_22-05-2026_SpringCore_Project-09,10,11,12&13/ElectricityBillGeneratorusingSetterInjection_Project-13/com.nit.beans/ElectricityBillGenerator.java
@@ -0,0 +1,19 @@
+package com.nit.beans;
+
+public class ElectricityBillGenerator {
+
+    private int unitsConsumed;
+    private double ratePerUnit;
+
+    public void setUnitsConsumed(int unitsConsumed) {
+        this.unitsConsumed = unitsConsumed;
+    }
+
+    public void setRatePerUnit(double ratePerUnit) {
+        this.ratePerUnit = ratePerUnit;
+    }
+
+    public double generateBill() {
+        return unitsConsumed * ratePerUnit;
+    }
+}
