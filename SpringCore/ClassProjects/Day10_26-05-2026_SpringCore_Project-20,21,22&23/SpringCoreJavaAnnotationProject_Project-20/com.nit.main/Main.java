dd0995a389aafbabec48c0dc3c9198e0e6ff86ec
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-06-11 23:35:45 +0530

    Add Main class for Spring application entry point
    Implemented main class to initialize Spring context and display hospital details.

---
+++ SpringCore/ClassProjects/Day10_26-05-2026_SpringCore_Project-20,21,22&23/SpringCoreJavaAnnotationProject_Project-20/com.nit.main/Main.java
@@ -0,0 +1,18 @@
+package com.nit.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.nit.beans.Hospital;
+import com.nit.config.AppConfig;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		Hospital h = (Hospital) context.getBean(Hospital.class);
+		h.display();
+
+	}
+
+}
