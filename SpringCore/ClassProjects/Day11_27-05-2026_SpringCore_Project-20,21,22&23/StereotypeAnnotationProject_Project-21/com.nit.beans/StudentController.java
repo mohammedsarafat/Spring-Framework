e95cc06d4b050ae8b49602e42316bf6299f3d5c4
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-06-12 22:51:43 +0530

    Create StudentController.java

---
+++ SpringCore/ClassProjects/Day11_27-05-2026_SpringCore_Project-20,21,22&23/StereotypeAnnotationProject_Project-21/com.nit.beans/StudentController.java
@@ -0,0 +1,11 @@
+package com.nit.beans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class StudentController {
+
+	public void getController() {
+		IO.println("This is a Controller Class");
+	}
+}
