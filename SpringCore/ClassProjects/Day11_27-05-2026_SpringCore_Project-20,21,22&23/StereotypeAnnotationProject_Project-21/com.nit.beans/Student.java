be06e755a8022a586b12de1f2a5260ae1662aab8
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-06-12 22:51:24 +0530

    Create Student.java

---
+++ SpringCore/ClassProjects/Day11_27-05-2026_SpringCore_Project-20,21,22&23/StereotypeAnnotationProject_Project-21/com.nit.beans/Student.java
@@ -0,0 +1,11 @@
+package com.nit.beans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+
+	public void studying() {
+		IO.println("This is a Student Class");
+	}
+}
