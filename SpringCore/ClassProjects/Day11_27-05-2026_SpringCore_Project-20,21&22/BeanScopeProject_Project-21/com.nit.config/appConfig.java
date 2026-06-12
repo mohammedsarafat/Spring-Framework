434ff85c732268c726a526873e1379f7f353ea50
Sarafat <mohammedsarafat786@gmail.com>
2026-06-12 23:12:57 +0530

    Update files

--- SpringCore/ClassProjects/Day11_27-05-2026_SpringCore_Project-20,21&22/BeanScopeProject_Project-21/com.nit.config/appConfig.java
+++ SpringCore/ClassProjects/Day11_27-05-2026_SpringCore_Project-20,21&22/BeanScopeProject_Project-21/com.nit.config/appConfig.java
@@ -5,6 +5,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "com.nit.beans")
-public class AppConfig {
+public class appConfig {
 
 }
