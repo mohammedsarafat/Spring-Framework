b66e3566fd7ebcf5f56ea8a336ad7b1d58e59f47
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-06-18 23:32:58 +0530

    Create Main.java

---
+++ SpringCore/LabProjects/Lab-Project02_ VehicleEngineSelectionSystem/com.nit.main/Main.java
@@ -0,0 +1,17 @@
+package com.nit.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.nit.beans.Vehicle;
+import com.nit.config.AppConfig;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		Vehicle v = context.getBean(Vehicle.class);
+		v.startVehicle();
+	}
+
+}
