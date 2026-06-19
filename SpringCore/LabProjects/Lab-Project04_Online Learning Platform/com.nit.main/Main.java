00b84a0a3066febb1e1f5650ce5bcce87cb25da3
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-06-19 23:51:34 +0530

    Create Main.java

---
+++ SpringCore/LabProjects/Lab-Project04_Online Learning Platform/com.nit.main/Main.java
@@ -0,0 +1,17 @@
+package com.nit.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.nit.beans.NotificationService;
+import com.nit.config.AppConfig;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		NotificationService n = context.getBean(NotificationService.class);
+		n.notifyUser("Meeting at 4 PM");
+	}
+
+}
