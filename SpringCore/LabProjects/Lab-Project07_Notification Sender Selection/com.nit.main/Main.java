e446e9554d6f4bdcbbfe3bb65ddea0860ada8b0a
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-06-24 23:39:01 +0530

    Add Main class for Notification Sender application

---
+++ SpringCore/LabProjects/Lab-Project07_Notification Sender Selection/com.nit.main/Main.java
@@ -0,0 +1,21 @@
+package com.nit.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.nit.center.ServiceCenter;
+import com.nit.config.AppConfig;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        ServiceCenter center =
+                context.getBean(ServiceCenter.class);
+
+        center.serviceVehicle();
+    }
+}
