153834507e89c614c67ba41f94d7e2bbdd6a565c
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-07-01 23:27:59 +0530

    Create Student.java

---
+++ SpringMVC/Class-Projects/Project-03_SpringMVC_FormHandling/com.nit.model/Student.java
@@ -0,0 +1,44 @@
+package com.nit.model;
+
+public class Student {
+
+    private String name;
+    private String email;
+    private String number;
+    private String branch;
+
+    public Student() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+}
