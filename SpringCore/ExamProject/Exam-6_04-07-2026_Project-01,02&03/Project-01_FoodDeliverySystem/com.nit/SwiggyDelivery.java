a5ff7b23c249a2413606712fce2bbd43d9f7b47e
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-07-04 21:36:50 +0530

    Create SwiggyDelivery.java

---
+++ SpringCore/ExamProject/Exam-6_04-07-2026_Project-01,02&03/Project-01_FoodDeliverySystem/com.nit/SwiggyDelivery.java
@@ -0,0 +1,12 @@
+package com.nit;
+
+public class SwiggyDelivery implements DeliveryService {
+
+    @Override
+    public void deliverOrder() {
+
+        System.out.println("Order is delivered using Swiggy Delivery.");
+
+    }
+
+}
