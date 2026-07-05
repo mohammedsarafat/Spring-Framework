4d60d34ab086b100ef8d4711decf3d1d9382c375
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-07-05 22:53:37 +0530

    Create Employee.java

---
+++ SpringCoreByPrashantSir/TaskProjects/Project-02_SpringCore_InjectionWithReferenceType/com.mds/Employee.java
@@ -0,0 +1,39 @@
+package com.mds;
+
+public class Employee
+{
+    private String name;
+    private int id;
+    private Address address;
+
+    public Employee()
+    {
+        System.out.println("Employee Object Created...");
+    }
+
+    public void setName(String name)
+    {
+        System.out.println("Employee name setter called");
+        this.name = name;
+    }
+
+    public void setId(int id)
+    {
+        System.out.println("Employee id setter called");
+        this.id = id;
+    }
+
+    public void setAddress(Address address)
+    {
+        System.out.println("Employee address setter called");
+        this.address = address;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Employee [name=" + name +
+               ", id=" + id +
+               ", " + address + "]";
+    }
+}
