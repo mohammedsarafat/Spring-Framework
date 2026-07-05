a116e5f422629980a86d6fc371ce06eefa57f848
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-07-05 22:53:21 +0530

    Create Address.java

---
+++ SpringCoreByPrashantSir/TaskProjects/Project-02_SpringCore_InjectionWithReferenceType/com.mds/Address.java
@@ -0,0 +1,31 @@
+package com.mds;
+
+public class Address
+{
+    private String city;
+    private int pincode;
+
+    public Address()
+    {
+        System.out.println("Address Object Created...");
+    }
+
+    public void setCity(String city)
+    {
+        System.out.println("Address city setter called");
+        this.city = city;
+    }
+
+    public void setPincode(int pincode)
+    {
+        System.out.println("Address pincode setter called");
+        this.pincode = pincode;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Address [city=" + city +
+               ", pincode=" + pincode + "]";
+    }
+}
