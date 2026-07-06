05b91bacf3457902a3acdaa4993b62a589ac45da
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-07-06 12:46:07 +0530

    Add Book class with title, price, and pages attributes

---
+++ SpringCoreByPrashantSir/TaskProjects/TaskProjec-Day11/com.nit.project01/Book.java
@@ -0,0 +1,39 @@
+package com.nit.project01;
+
+public class Book
+{
+    private String title;
+    private int price;
+    private int pages;
+
+    public Book()
+    {
+        System.out.println("Book Object Created...");
+    }
+
+    public void setTitle(String title)
+    {
+        System.out.println("Book title setter called");
+        this.title = title;
+    }
+
+    public void setPrice(int price)
+    {
+        System.out.println("Book price setter called");
+        this.price = price;
+    }
+
+    public void setPages(int pages)
+    {
+        System.out.println("Book pages setter called");
+        this.pages = pages;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Book [title=" + title +
+               ", price=" + price +
+               ", pages=" + pages + "]";
+    }
+}
