33223f1c7a8835faec93af1b92b3fdaeb24ff051
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-07-11 22:48:25 +0530

    Create ViceChancellor.java

---
+++ SpringCoreByPrashantSir/ExamProject/Exam-01_11-07-2026_Project-01,02,03&04/com.nit.project01/ViceChancellor.java
@@ -0,0 +1,30 @@
+package com.nit.project01;
+
+public class ViceChancellor {
+
+    private String name;
+    private String qualification;
+    private int experience;
+
+    public ViceChancellor() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    @Override
+    public String toString() {
+        return "\nName          : " + name
+                + "\nQualification : " + qualification
+                + "\nExperience    : " + experience + " Years";
+    }
+}
