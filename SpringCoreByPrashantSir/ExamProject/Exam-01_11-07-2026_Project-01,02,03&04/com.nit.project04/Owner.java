cbeae4db2cc3dc9afdde9734448005a6c736b24a
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-07-12 22:45:15 +0530

    Create Owner.java

---
+++ SpringCoreByPrashantSir/ExamProject/Exam-01_11-07-2026_Project-01,02,03&04/com.nit.project04/Owner.java
@@ -0,0 +1,23 @@
+package com.nit.project04;
+
+public class Owner {
+
+    private int ownerId;
+    private String ownerName;
+    private String contactNumber;
+
+    public Owner(int ownerId, String ownerName, String contactNumber) {
+
+        this.ownerId = ownerId;
+        this.ownerName = ownerName;
+        this.contactNumber = contactNumber;
+    }
+
+    @Override
+    public String toString() {
+
+        return ownerName +
+                " (ID : " + ownerId +
+                ", Contact : " + contactNumber + ")";
+    }
+}
