dd66c9551405bbec0fd6e12bbda1ea676ffdef39
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-07-14 22:38:27 +0530

    Create EmployeeRepository.java

---
+++ SpringBoothWithRestApiByMam/Project-02_SpringbootRestApi_EmployeeManagementSystem/com.nit.Project_02_SpringbootRestApi_EmployeeManagementSystem.repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.nit.Project_02_SpringbootRestApi_EmployeeManagementSystem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nit.Project_02_SpringbootRestApi_EmployeeManagementSystem.enitity.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
