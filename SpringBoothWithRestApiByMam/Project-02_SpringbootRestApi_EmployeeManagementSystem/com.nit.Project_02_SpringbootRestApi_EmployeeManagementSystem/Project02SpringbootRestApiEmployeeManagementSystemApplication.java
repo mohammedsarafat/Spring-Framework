9bd2a8d49966ef9cfc0f278b5f9c9336156c40fd
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-07-14 22:36:15 +0530

    Create Project02SpringbootRestApiEmployeeManagementSystemApplication.java

---
+++ SpringBoothWithRestApiByMam/Project-02_SpringbootRestApi_EmployeeManagementSystem/com.nit.Project_02_SpringbootRestApi_EmployeeManagementSystem/Project02SpringbootRestApiEmployeeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.nit.Project_02_SpringbootRestApi_EmployeeManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project02SpringbootRestApiEmployeeManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project02SpringbootRestApiEmployeeManagementSystemApplication.class, args);
+	}
+
+}
