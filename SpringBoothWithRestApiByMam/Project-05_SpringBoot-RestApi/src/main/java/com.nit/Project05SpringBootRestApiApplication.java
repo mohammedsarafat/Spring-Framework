fc95cbc3324e4a414002637db6d3314da38fd26c
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-07-24 23:43:46 +0530

    Create Project05SpringBootRestApiApplication.java

---
+++ SpringBoothWithRestApiByMam/Project-05_SpringBoot-RestApi/src/main/java/com.nit/Project05SpringBootRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project05SpringBootRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project05SpringBootRestApiApplication.class, args);
+	}
+
+}
