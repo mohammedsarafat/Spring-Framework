7a2bc1c102a8148c7ac4cad9b31a65f5114d8032
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-07-25 22:03:07 +0530

    Create MainApp.java

---
+++ SpringCoreByPrashantSir/ExamProExam-03_25-07-2026_Project-010203/src/main/java/com.nit.project02/MainApp.java
@@ -0,0 +1,16 @@
+package com.nit.project02;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HotelBooking  hb = context.getBean(HotelBooking.class);
+        hb.displayBill();
+    }
+}
