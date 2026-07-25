2fdfdf36edc8a4b78f70361b88a46aaf7329c59d
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-07-25 22:02:34 +0530

    Create AppConfig.java

---
+++ SpringCoreByPrashantSir/ExamProExam-03_25-07-2026_Project-010203/src/main/java/com.nit.project02/AppConfig.java
@@ -0,0 +1,11 @@
+package com.nit.project02;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan("com.nit.project02")
+@PropertySource("classpath:com/nit/project02/application.properties")
+public class AppConfig {
+	
+}
