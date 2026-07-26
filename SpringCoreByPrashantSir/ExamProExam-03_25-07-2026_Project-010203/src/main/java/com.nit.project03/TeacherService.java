f8cfbb568f5db765abf1c4341e8a8f3e510f1cd6
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-07-26 23:32:27 +0530

    Create TeacherService.java

---
+++ SpringCoreByPrashantSir/ExamProExam-03_25-07-2026_Project-010203/src/main/java/com.nit.project03/TeacherService.java
@@ -0,0 +1,11 @@
+package com.nit.project03;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TeacherService {
+
+    public TeacherService() {
+        System.out.println("TeacherService Bean Created");
+    }
+}
