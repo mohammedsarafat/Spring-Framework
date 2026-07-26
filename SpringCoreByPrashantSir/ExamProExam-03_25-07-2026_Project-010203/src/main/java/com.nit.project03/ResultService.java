6cfd004b5ff7ab286049b1d352313d2f9ecf76d4
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-07-26 23:31:54 +0530

    Create ResultService.java

---
+++ SpringCoreByPrashantSir/ExamProExam-03_25-07-2026_Project-010203/src/main/java/com.nit.project03/ResultService.java
@@ -0,0 +1,17 @@
+package com.nit.project03;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+@Lazy
+public class ResultService {
+
+    public ResultService() {
+        System.out.println("ResultService Bean Created");
+    }
+
+    public void generateResult() {
+        System.out.println("Student Result Generated Successfully");
+    }
+}
