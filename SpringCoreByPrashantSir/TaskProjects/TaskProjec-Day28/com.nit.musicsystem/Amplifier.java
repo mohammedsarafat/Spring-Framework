f5150241b2d056ec2981d54d00517d68d523e636
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-07-27 22:51:57 +0530

    Create Amplifier.java

---
+++ SpringCoreByPrashantSir/TaskProjects/TaskProjec-Day28/com.nit.musicsystem/Amplifier.java
@@ -0,0 +1,12 @@
+package com.nit.musicsystem;
+
+public class Amplifier {
+
+    public Amplifier() {
+        System.out.println("Amplifier Constructor");
+    }
+
+    public void boostSignal() {
+        System.out.println("Signal boosted by amplifier");
+    }
+}
