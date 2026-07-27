6bb65d6528c6358aeb5c2313565f6e3322f59452
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-07-27 22:50:45 +0530

    Create Speaker.java

---
+++ SpringCoreByPrashantSir/TaskProjects/TaskProjec-Day28/com.nit.musicsystem/Speaker.java
@@ -0,0 +1,11 @@
+package com.nit.musicsystem;
+public class Speaker {
+
+    public Speaker() {
+        System.out.println("Speaker Constructor");
+    }
+
+    public void playSound() {
+        System.out.println("Sound is playing through speaker");
+    }
+}
