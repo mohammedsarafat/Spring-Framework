0ce64fcc45cfc1071432af52cc25002c9de84f28
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-07-28 23:14:41 +0530

    Create AppConfig.java

---
+++ SpringCoreByPrashantSir/TaskProjects/TaskProjec-Day28/com.nit.employee3/AppConfig.java
@@ -0,0 +1,9 @@
+package com.nit.employee3;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.nit.employee3")
+public class AppConfig {
+}
