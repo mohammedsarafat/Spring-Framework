a4ac967756162705ee272efca2d411e5a4cdd680
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-07-28 23:30:50 +0530

    Create Main.java

---
+++ SpringCoreByPrashantSir/TaskProjects/TaskProjec-Day28/com.nit.employee4/Main.java
@@ -0,0 +1,19 @@
+package com.nit.employee4;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+
+        Employee employee = context.getBean(Employee.class);
+
+        System.out.println(employee);
+        
+    }
+}
