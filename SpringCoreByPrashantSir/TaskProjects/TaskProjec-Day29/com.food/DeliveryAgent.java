7ea4603643f540682bce2f1af3f793ce392013a1
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-07-29 18:59:32 +0530

    Create DeliveryAgent.java

---
+++ SpringCoreByPrashantSir/TaskProjects/TaskProjec-Day29/com.food/DeliveryAgent.java
@@ -0,0 +1,17 @@
+package com.food;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DeliveryAgent {
+
+    @Autowired
+    private Restaurant restaurant;
+
+    public void deliverOrder() {
+        System.out.println("Delivery Agent received the order.");
+        restaurant.prepareFood();
+        System.out.println("Order Delivered Successfully.");
+    }
+}
