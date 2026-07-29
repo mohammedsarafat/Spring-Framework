521553e3433c180853b830d1935831a6d25f73cb
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-07-29 18:59:44 +0530

    Create AppConfig.java

---
+++ SpringCoreByPrashantSir/TaskProjects/TaskProjec-Day29/com.food/AppConfig.java
@@ -0,0 +1,9 @@
+package com.food;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.food")
+public class AppConfig {
+}
