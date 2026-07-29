312459a52944711681673a17a40739700be36aa5
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-07-29 18:58:27 +0530

    Create AppConfig.java

---
+++ SpringCoreByPrashantSir/TaskProjects/TaskProjec-Day29/com.hospital/AppConfig.java
@@ -0,0 +1,9 @@
+package com.hospital;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.hospital")
+public class AppConfig {
+}
