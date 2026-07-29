ef4fe90736a0bcc880a83fcdf78c572807bd0677
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-07-29 18:57:59 +0530

    Add Doctor class for patient consultation

---
+++ SpringCoreByPrashantSir/TaskProjects/TaskProjec-Day29/com.hospital/Doctor.java
@@ -0,0 +1,16 @@
+package com.hospital;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Doctor {
+
+    @Autowired
+    private Patient patient;
+
+    public void consult() {
+        System.out.println("Doctor is consulting the patient.");
+        patient.getPatientDetails();
+    }
+}
