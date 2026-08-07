7f2b115bb9448a63972a2523e16fea1004f15274
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-08-07 23:06:00 +0530

    Create OperationConfig.java

---
+++ SpringCoreByPrashantSir/TaskProjects/TaskProjec-Day38/com.producttrackingproject/OperationConfig.java
@@ -0,0 +1,40 @@
+package com.producttrackingproject;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+@ComponentScan("com.producttrackingproject")
+@PropertySource("classpath:application.properties")
+public class OperationConfig {
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public DataSource dataSource() {
+
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+
+        ds.setDriverClassName(environment.getProperty("db.driverName"));
+        ds.setUrl(environment.getProperty("db.url"));
+        ds.setUsername(environment.getProperty("db.username"));
+        ds.setPassword(environment.getProperty("db.password"));
+
+        return ds;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+
+}
