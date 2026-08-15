a1fe83471be36200cd185db53db67b0d98285c8a
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-08-15 23:18:15 +0530

    Create Department.java

---
+++ SpringCoreByPrashantSir/LabProject/LabProject-Day05_SpringCore-CustomAnnotationPractice/src/main/java/com.nit.task03/Department.java
@@ -0,0 +1,21 @@
+package com.nit.task03;
+
+public class Department {
+
+    private String deptName;
+    private String location;
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return "Department [deptName=" + deptName
+                + ", location=" + location + "]";
+    }
+}
