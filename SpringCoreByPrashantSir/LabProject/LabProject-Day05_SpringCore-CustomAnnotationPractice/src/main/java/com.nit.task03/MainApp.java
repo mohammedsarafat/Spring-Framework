0348051577aa8c60448b4d98fa93134162a803c2
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-08-15 23:18:50 +0530

    Create MainApp.java

---
+++ SpringCoreByPrashantSir/LabProject/LabProject-Day05_SpringCore-CustomAnnotationPractice/src/main/java/com.nit.task03/MainApp.java
@@ -0,0 +1,17 @@
+package com.nit.task03;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("com/nit/task03/applicationContext.xml");
+
+        Employee employee = context.getBean("employee", Employee.class);
+
+        System.out.println(employee);
+    }
+}
