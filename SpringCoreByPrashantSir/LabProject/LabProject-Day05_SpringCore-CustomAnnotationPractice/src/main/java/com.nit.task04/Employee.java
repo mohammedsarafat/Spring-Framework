926a9e4bfc58f35cd05703659f6471d6e2b69c6c
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-08-16 23:03:42 +0530

    Create Employee.java

---
+++ SpringCoreByPrashantSir/LabProject/LabProject-Day05_SpringCore-CustomAnnotationPractice/src/main/java/com.nit.task04/Employee.java
@@ -0,0 +1,34 @@
+package com.nit.task04;
+
+public class Employee {
+
+    private int id;
+    private String name;
+    private double salary;
+
+    private Department department;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [id=" + id
+                + ", name=" + name
+                + ", salary=" + salary
+                + ", department=" + department + "]";
+    }
+}
