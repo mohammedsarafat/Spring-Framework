75c7c722e75aca2e45cf7bb5a1dc952c57813290
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-08-20 10:05:36 +0530

    Create Patient.java

---
+++ SpringCoreByPrashantSir/LabProject/LabProject-Day06_SpringCore-AutowiringPracticeTasks/src/main/java/com.nit.hospital/Patient.java
@@ -0,0 +1,11 @@
+package com.nit.hospital;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Patient {
+
+    public void treatment() {
+        System.out.println("Patient is under treatment");
+    }
+}
