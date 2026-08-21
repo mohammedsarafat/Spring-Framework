e41d64d3efb9a0d98bb6c997e97956e77cb6c039
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-08-21 22:43:52 +0530

    Create Book.java

---
+++ SpringCoreByPrashantSir/LabProject/LabProject-Day06_SpringCore-AutowiringPracticeTasks/src/main/java/com.nit.library/Book.java
@@ -0,0 +1,11 @@
+package com.nit.library;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Book {
+
+    public void bookInfo() {
+        System.out.println("Book Issued Successfully");
+    }
+}
