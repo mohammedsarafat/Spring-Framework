838a5330d27314e6e5bdf6e7bd7c1da3fb717724
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-08-23 23:05:31 +0530

    Create EmployeeService.java

---
+++ SpringCoreByPrashantSir/LabProject/LabProject-Day07_SpringCore-Component/src/main/java/com.nit.employee/EmployeeService.java
@@ -0,0 +1,11 @@
+package com.nit.employee;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmployeeService {
+
+    public void displayEmployeeDetails() {
+        System.out.println("Employee details loaded successfully.");
+    }
+}
