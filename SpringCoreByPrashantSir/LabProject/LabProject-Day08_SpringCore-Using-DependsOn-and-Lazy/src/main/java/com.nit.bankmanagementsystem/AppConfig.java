2b711420fd5e79dcc29c9799cc62d3a79b04a93b
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-08-27 22:38:55 +0530

    Add AppConfig class for Spring configuration

---
+++ SpringCoreByPrashantSir/LabProject/LabProject-Day08_SpringCore-Using-DependsOn-and-Lazy/src/main/java/com.nit.bankmanagementsystem/AppConfig.java
@@ -0,0 +1,10 @@
+package com.nit.bankmanagementsystem;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.nit.bankmanagementsystem")
+public class AppConfig {
+
+}
