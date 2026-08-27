13a2aa53ed3f11aa329fd451931e1554fb6248ca
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-08-27 22:47:38 +0530

    Add AppConfig class for Spring configuration

---
+++ SpringCoreByPrashantSir/LabProject/LabProject-Day08_SpringCore-Using-DependsOn-and-Lazy/src/main/java/com.nit.smartparkingmanagementsystem/AppConfig.java
@@ -0,0 +1,10 @@
+package com.nit.smartparkingmanagementsystem;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.nit.smartparkingmanagementsystem")
+public class AppConfig {
+
+}
