066573973de76a909de0b21a69b58f223fa5706a
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-09-05 17:05:08 +0530

    Add AppConfig class for Spring component scanning

---
+++ SpringCoreByPrashantSir/LabProject/LabProject-Day11_SpringCore-UsingComponentScanAndAutowiring/src/main/java/com.nit.hospital/AppConfig.java
@@ -0,0 +1,10 @@
+package com.nit.hospital;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.nit.hospital")
+public class AppConfig {
+
+}
