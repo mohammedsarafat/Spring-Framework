e7880ee7563d0762b00fbe3da48353d9cdffef86
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-09-05 17:04:07 +0530

    Add Restaurant class with food preparation method

---
+++ SpringCoreByPrashantSir/LabProject/LabProject-Day11_SpringCore-UsingComponentScanAndAutowiring/src/main/java/com.nit.food/Restaurant.java
@@ -0,0 +1,12 @@
+package com.nit.food;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Restaurant {
+
+    public void prepareFood() {
+
+        System.out.println("Restaurant prepared Veg Biryani.");
+    }
+}
