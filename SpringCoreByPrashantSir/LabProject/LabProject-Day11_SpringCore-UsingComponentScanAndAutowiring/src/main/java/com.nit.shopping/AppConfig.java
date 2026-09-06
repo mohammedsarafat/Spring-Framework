08a46e69abf1851d77223a9742889eeb2df662c7
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-09-06 16:06:02 +0530

    Add AppConfig class for Spring configuration

---
+++ SpringCoreByPrashantSir/LabProject/LabProject-Day11_SpringCore-UsingComponentScanAndAutowiring/src/main/java/com.nit.shopping/AppConfig.java
@@ -0,0 +1,10 @@
+package com.nit.shopping;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.nit.shopping")
+public class AppConfig {
+
+}
