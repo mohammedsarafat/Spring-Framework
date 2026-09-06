dc8818b5f86f2d04af52d2baf28180a6671a9329
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-09-06 16:04:03 +0530

    Add Patient class with details method

---
+++ SpringCoreByPrashantSir/LabProject/LabProject-Day11_SpringCore-UsingComponentScanAndAutowiring/src/main/java/com.nit.hospital/Patient.java
@@ -0,0 +1,13 @@
+package com.nit.hospital;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Patient {
+
+    public void getPatientDetails() {
+
+        System.out.println("Patient Name: Rahul");
+        System.out.println("Disease: Fever");
+    }
+}
