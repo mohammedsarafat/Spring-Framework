ef686f21e961ba36afa924b6fbdbe0d6f5f29d79
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-09-06 16:07:28 +0530

    Add OrderService class with order placement functionality

---
+++ SpringCoreByPrashantSir/LabProject/LabProject-Day11_SpringCore-UsingComponentScanAndAutowiring/src/main/java/com.nit.shopping/OrderService.java
@@ -0,0 +1,20 @@
+package com.nit.shopping;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderService {
+
+    @Autowired
+    private PaymentService paymentService;
+
+    public void placeOrder() {
+
+        System.out.println("Order Placed.");
+
+        paymentService.makePayment();
+
+        System.out.println("Thank you for Shopping.");
+    }
+}
