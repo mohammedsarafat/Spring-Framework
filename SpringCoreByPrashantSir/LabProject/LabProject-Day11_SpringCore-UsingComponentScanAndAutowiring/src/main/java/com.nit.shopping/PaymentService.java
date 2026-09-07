c1214ff025af8e458e106bcfcd5fa7831b94576f
mohammedsarafat <mohammedsarafat786@gmail.com>
2026-09-07 22:26:53 +0530

    Add PaymentService class for handling payments

---
+++ SpringCoreByPrashantSir/LabProject/LabProject-Day11_SpringCore-UsingComponentScanAndAutowiring/src/main/java/com.nit.shopping/PaymentService.java
@@ -0,0 +1,12 @@
+package com.nit.shopping;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentService {
+
+    public void makePayment() {
+
+        System.out.println("Payment Completed Successfully.");
+    }
+}
